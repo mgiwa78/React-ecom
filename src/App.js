6d332be6ca63f30111248266e7965fa1bddc380d
Giwa Muhammad <mgiwa78@gmail.com>
2022-02-25 15:54:38 +0100

    Created Header component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import React from "react";
 import { BrowserRouter, Route, Routes, Switch } from "react-router-dom";
 import Homepage from "./pages/homepage/home-page.component";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 
 function App() {
   return (
     <div>
+      <Header />
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/shop" element={<ShopPage />} />
