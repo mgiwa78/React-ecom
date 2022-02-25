d5d9af61f948e73a653596a7a47d244ac90eaad4
Giwa Muhammad <mgiwa78@gmail.com>
2022-02-25 03:50:44 +0100

    some routing stuff

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import "./App.css";
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import Homepage from "./pages/homepage/home-page.component";
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Route path="/hat">
+        <HatsPage />
+      </Route>
+      <Route path="/">
+        <Homepage />
+      </Route>
     </div>
   );
 }
